65f5c95f3549ebccbe963275f8fdc12b946156eb
Max Burkanov <mburkanov@highresbio.com>
2021-12-05 15:58:09 -0500

    update readme / add tsconfig

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-### Phone formatter for kyrgyzstan phone numbers
+## Phone formatter for kyrgyzstan phone numbers
+
+Input that only allows numbers, and formats the input text to format as you type.
+
+## Usage
+
+```shell script
+yarn add phone-formatter-kyrgyz
+```
+
+```shell script
+npm install phone-formatter-kyrgyz
+```
+
+```typescript
+import { formatNumber } from "phone-formatter-kyrgyz";
+
+const [input, setInput] = React.Usestate<string>("");
+
+const onChange = (e) => {
+  const value = e.target.value;
+  const result = formatNumber(value);
+
+  setInput(result);
+};
+```
+
+```jsx
+<input onChange={onChange} value={input}>
+```
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export const formatNumber = (value: string): string => {
+  if (value[0] === "(" && value.length === 1) return "";
+  const incoming = value.replace(/\D/g, "");
+  const input = incoming.substring(0, 10);
+  const areaCode = input.substring(0, 4);
+  const start = input.substring(4, 6);
+  const mid = input.substring(6, 8);
+  const last = input.substring(8, 10);
+
+  if (input.length > 8) {
+    return `(${areaCode}) ${start} - ${mid} - ${last}`;
+  } else if (input.length > 6) {
+    return `(${areaCode}) ${start} - ${mid}`;
+  } else if (input.length > 4) {
+    return `(${areaCode}) ${start}`;
+  } else if (input.length > 0) {
+    return `(${areaCode}`;
+  }
+  return "";
+};
